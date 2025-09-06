7011b6b63f186a8e6224203cc26a0aee1b805b25
shariarzaman <shariarzaman11@gmail.com>
2025-09-06 19:44:27 +0000

    Update MCM

--- Matrix/mcm.cpp
+++ Matrix/mcm.cpp
@@ -1,64 +1,47 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n;
-    cout << "How many matrices are there?: ";
-    cin >> n;
-
-    int dim[n+1];   // dimensions
-    cout << "Matrix Dimensions: ";
-    for(int i=0; i<=n; i++) {
-        cin >> dim[i];
-    }
-
-    // DP table - initialize with -1 to identify uncomputed cells
-    int cost[n][n];
     
-    // Initialize all elements to -1 first
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cost[i][j] = -1;
-        }
+    int d[]={30,35,15,5,10,20,25};
+    
+    int x=sizeof(d)/sizeof(d[0]);
+    int s=x-1;
+    int arr[s][s];
+    
+     
+    for(int i=0;i<s;i++){
+        arr[i][i]=0;
     }
     
-    for(int i=0; i<n; i++){ // Diagonal elements
-        cost[i][i] = 0;
-    }
-
-    cout << endl;
-
-    for(int len=2; len<=n; len++) {       // Matrix length
-        for(int i=0; i<=n-len; i++) {     // Start index
-            int j = i + len - 1;          // End index
-            cost[i][j] = 999999;
-
-            for(int k=i; k<j; k++) {      // Where to split
-                // Cost calculate
-                int current_cost = cost[i][k] + cost[k+1][j] +
-                                  dim[i] * dim[k+1] * dim[j+1];
-
-                // Minimum cost update
-                if(current_cost < cost[i][j]) {
-                    cost[i][j] = current_cost;
-                }
+    
+    for(int l=1;l<s;l++){
+        for(int i=0;i<s-1;i++){
+        for(int j=i+l;j<s;j++){
+            int cnt=9999999;
+            
+            for(int k=i;k<j;k++){
+                arr[i][j]=arr[i][k]+arr[k+1][j]+(d[i]*d[k+1]*d[j+1]);
+                cout<<cnt<<" ";
+                cout<<arr[i][j]<<endl;
+                
+                cnt=min(cnt,arr[i][j]);
+                arr[i][j]=cnt;
+                // cout<<d[j+1]<<endl;
+                // cout<<d[i]<<d[k+1]<<d[j+1]<<endl;
+                
             }
+                cout<<arr[i][j]<<endl;
+            
+            break;
         }
+        cout<<endl;
     }
-
-    cout << "Minimum multiplication needed: " << cost[0][n-1] << endl << endl;
+    }
+    cout<<arr[0][5]<<endl;
     
-    cout << "Cost matrix:" << endl;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(cost[i][j] >= 0 && cost[i][j] != 999999) {
-                cout << cost[i][j] << " ";
-            } else {
-                cout << "- ";  // Display null/uncomputed values as "-"
-            }
-        }
-        cout << endl;
-    }
 
-    return 0;
+
+
+
 }
