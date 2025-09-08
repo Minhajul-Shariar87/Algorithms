baa1a0fd627f237a72dd600aa65c92ee80d0d570
shariarzaman <shariarzaman11@gmail.com>
2025-09-08 12:47:08 +0000

    Error Fixing

--- Matrix/lcs1.cpp
+++ Matrix/lcs1.cpp
@@ -3,39 +3,33 @@
 using namespace std;
 
 int main(){
-    char a[]={'a','b','b','c','a','a','c'};
-    char b[]={'a','c','c','b','c','c','a'};
+    char a[]={'A','X','T','Y'};
+    char b[]={'A','Y','Z','X'};
 
     int s1= sizeof(a)/sizeof(a[0]);
     int s2= sizeof(b)/sizeof(b[0]);
+
+    
 
     int arr[s1+1][s2+1];
 
     for(int i=0;i<s1+1;i++){
         for(int j=0;j<s2+1;j++){
-            if(i==0){
-                arr[i][j]=0;
-            }else{
-                arr[i][j]=0;
-            }
-            break;
+            
+            arr[i][j]=0;
+
         }
     }
 
-    for(int i=0;i<s1+1;i++){
-    for(int j=0;j<s2+1;j++){
-        cout<<arr[i][j]<<" ";
-    }
-    cout<<endl;
-    }
-    cout<<endl;
+    
+    
 
     for(int i=1;i<s1+1;i++){
         for(int j=1;j<s2+1;j++){
-            // cout<<a[i-1]<<b[j-1]<<endl;
-            if(a[i-1]==b[j-1]){
+            
+            if(b[j-1]==a[i-1]){
                 arr[i][j]=arr[i-1][j-1]+1;
-                // cout<<arr[i][j]<<endl;
+                
             }else{
                 arr[i][j]=max(arr[i][j-1],arr[i-1][j]);
             }
--- Matrix/mcm.cpp
+++ Matrix/mcm.cpp
@@ -9,11 +9,25 @@ int main(){
     int s=x-1;
     int arr[s][s];
     
-     
+    
     for(int i=0;i<s;i++){
         arr[i][i]=0;
     }
-    
+
+    for(int i=0;i<s;i++){
+        for(int j=0;j<s;j++){
+        if(i!=j){
+        arr[i][j]=45;
+        }
+        }
+    }
+    for(int i=0;i<s;i++){
+        for(int j=0;j<s;j++){
+        cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     
     for(int l=1;l<s;l++){
         for(int i=0;i<s-1;i++){
@@ -22,23 +36,34 @@ int main(){
             
             for(int k=i;k<j;k++){
                 arr[i][j]=arr[i][k]+arr[k+1][j]+(d[i]*d[k+1]*d[j+1]);
-                cout<<cnt<<" ";
-                cout<<arr[i][j]<<endl;
                 
                 cnt=min(cnt,arr[i][j]);
                 arr[i][j]=cnt;
-                // cout<<d[j+1]<<endl;
-                // cout<<d[i]<<d[k+1]<<d[j+1]<<endl;
+                
                 
             }
-                cout<<arr[i][j]<<endl;
+                
             
             break;
         }
+        
+    }  
+    }
+    for(int i=0;i<s;i++){
+        for(int j=0;j<s;j++){
+            if(i!=j && i>j){
+        cout<<static_cast<char>(arr[i][j])<<" ";
+            }else{
+                cout<<arr[i][j]<<" ";
+            }
+        }
         cout<<endl;
     }
-    }
-    cout<<arr[0][5]<<endl;
+
+    cout<<arr[0][s-1];
+
+    
+    
     
 
 
