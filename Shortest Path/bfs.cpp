c8cee10e28dae1d2ca0acf7e4328a147fc4fe02a
shariarzaman <shariarzaman11@gmail.com>
2025-09-19 14:59:27 +0000

    Bfs moved

--- Shortest Path/bfs.cpp
+++ Shortest Path/bfs.cpp
@@ -1,60 +1,85 @@
-#include <iostream>
-#include <queue>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-void BFS(int G[][100], int n, int s) {
-    bool visited[100] = {false}; // visited array
-    queue<int> Q;
 
-    Q.push(s);
-    visited[s] = true;
 
-    bool first = true; // শুধু প্রিন্ট ফরম্যাট ঠিক রাখার জন্য
+void addedges(){}
 
-    while (!Q.empty()) {
-        int v = Q.front();
-        Q.pop();
 
-        if (first) {
-            cout << v;
-            first = false;
-        } else {
-            cout << " " << v;
+int main(){
+    
+    int edges,vertices;
+    cout<<"Enter no of edges: ";
+    cin>>edges;
+    cout<<"Enter no of vertices: ";
+    cin>>vertices;
+    vertices+=1;
+    
+    
+    cout<<"Enter no of edges: ";
+    
+    
+    int bfs[vertices][vertices];
+    for(int i=0;i<vertices;i++){
+        for(int j=0;j<vertices;j++){
+            bfs[i][j]=0;
+        }
+    }
+
+
+    
+        for(int j=1;j<=edges;j++){
+            int p,c;
+            cin>>p>>c;
+            bfs[p][c]=1;
+            bfs[c][p]=1;
+        
+            
         }
 
-        // process all neighbours of v
-        for (int w = 0; w < n; w++) {
-            if (G[v][w] == 1 && !visited[w]) {
-                Q.push(w);
-                visited[w] = true;
+    
+
+    for(int i=0;i<vertices;i++){
+        for(int j=0;j<vertices;j++){
+            cout<<bfs[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int start;
+
+    cout<<"Enter which to start: ";
+    cin>>start;
+
+    int cnt=0;
+    int cnt1=0;
+    int q[vertices];
+    cout<<start<<" ";
+
+    int i=start;
+    while(i<vertices){
+        for(int j=0;j<vertices;j++){
+            if(bfs[i][j]==1){
+                q[cnt1]=j;
+                cnt1++;
+                
+                cout<<j<<" ";
+                bfs[i][j]=0;
+                bfs[j][i]=0;
+
+                
             }
         }
-    }
-}
-
-int main() {
-    int n, e;
-    cout << "Enter number of vertices: ";
-    cin >> n;
-    cout << "Enter number of edges: ";
-    cin >> e;
-
-    int G[100][100] = {0}; // adjacency matrix (size fixed max 100)
-
-    cout << "Enter edges (u v):\n";
-    for (int i = 0; i < e; i++) {
-        int u, v;
-        cin >> u >> v;
-        G[u][v] = 1;
-        G[v][u] = 1; // যদি graph undirected হয়
+        i=q[cnt];
+        cnt++;
     }
 
-    int source;
-    cout << "Enter source node: ";
-    cin >> source;
 
-    cout << "BFS traversal: ";
-    BFS(G, n, source);
 
-    return 0;
+
+    
+
+
+
+
 }
