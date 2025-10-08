f1ea7b05e2e6fbd468819139b3a5ed4708c800b9
Minhajul Alif <shariarzaman11@gmail.com>
2025-10-08 13:05:33 +0000

    STill done

--- Prims.cpp
+++ Prims.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main(){
     
-    int v,e;
+    int v,e,value;
     char f,t;
     cout<<"Enter no of vertices: ";
     cin>>v;
+    vector<int>store;
     char arr1[v][v];
     int arr[v][v];
+    vector<int>prims;
+    int store[v];
+    int cnt=0;
     int fcnt,tcnt;
     for(int i=0;i<v;i++){
         for(int j=0;j<v;j++){
@@ -24,17 +28,17 @@ int main(){
         }
         cout<<endl;
     }
-    cout<<"Enter no of edges: ";
+    cout<<"Enter no of edges and value: ";
     cin>>e;
     
     for(int i=1;i<=e;i++){
-        cin>>f>>t;
+        cin>>f>>t>>value;
         fcnt=f-'A';
         
         tcnt=t-'A';
        
-        arr[fcnt][tcnt]=1;
-        arr[tcnt][fcnt]=1;
+        arr[fcnt][tcnt]=value;
+        arr[tcnt][fcnt]=value;
         
     }
     for(int i=0;i<v;i++){
@@ -44,7 +48,43 @@ int main(){
         }
         cout<<endl;
     }
-    int start;
-    cout<<"Enter "
+    char start;
+    cout<<"Enter which vertices to start: ";
+    cin>>start;
+    int i=start-'A';
+    
+    int cnt=0,value1=0;
+    int nmin=999999999;
+     for(int k=0;k<v;k++){
+        if(arr[i][k]!=0){
+        
+         if(nmin<arr[i][k]){
+            nmin=arr[i][k];
+            i=k;
+            prims[cnt]=nmin;
+         }else{
+            store.push_back(arr[i][k]);
+         }
+        }
+
+     }
+    while(i<v){
+        int nmin=999999999;
+        for(int k=0;k<store.size();k++){
+            if(arr[i][k]!=0){
+        
+         if(nmin<arr[i][k]){
+            nmin=arr[i][k];
+            i=k;
+            prims[cnt]=nmin;
+         }else{
+            store.push_back(arr[i][k]);
+         }
+        }
+        }
+        
+    }
+
+
     
 }
